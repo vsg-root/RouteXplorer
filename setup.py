ff9091c2652b956390b91a049cb3a4c55731247f
Vinícius Gomes <vsg.dev.tech@gmail.com>
2023-06-08 10:38:02 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='routexplorer',
+    version='0.0.0',
+    description='Biblioteca para Otimização de Rotas em Grafos Completos.',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'routexplorer = routexplorer.__main__:main'
+        ]
+    },
+    install_requires=[
+        # Dependências do seu projeto
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
 
