8c0d5528b8f2d05c134aa96da24f8919ededd9af
Vinícius Santos <viniciussantoslima2003@gmail.com>
2023-07-25 17:24:50 -0300

    Update execute.py

--- experiments/execute.py
+++ experiments/execute.py
@@ -1,10 +1,13 @@
 from utils.input_reader import InputReader
+from utils.Graph import Graph
 from algorithms.BruteForceSearch import BruteForceSearch
 
 # Datas of graph
-graph = InputReader.read_file(input("Enter file path: ")) #./experiments/input.txt
+graph = Graph(InputReader.get_vertices(InputReader.get_map_size(input()), input_function=input))
 
 # Calculation of the best path and distance
-best_path, best_distance = BruteForceSearch.find_best_path(graph)
+best_path, _ = BruteForceSearch.find_best_path(graph)
 
-print(" ".join(best_path))
+path_length = len(best_path)
+for i in range(path_length):
+    print(best_path[i], end=" " if i < path_length - 1 else "\n")
